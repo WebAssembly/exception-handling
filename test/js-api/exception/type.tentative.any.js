b28276afcee10c236a02f25c1d95e8fe792c5173
Ms2ger <Ms2ger@gmail.com>
2021-06-25 10:17:21 +0200

    Reindent.

--- test/js-api/exception/type.tentative.any.js
+++ test/js-api/exception/type.tentative.any.js
@@ -2,20 +2,20 @@
 // META: script=/wasm/jsapi/assertions.js
 
 function assert_type(argument) {
-    const exception = new WebAssembly.Exception(argument);
-    const exceptiontype = exception.type();
+  const exception = new WebAssembly.Exception(argument);
+  const exceptiontype = exception.type();
 
-    assert_array_equals(exceptiontype.parameters, argument.parameters);
+  assert_array_equals(exceptiontype.parameters, argument.parameters);
 }
 
 test(() => {
-    assert_type({ "parameters": [] });
+  assert_type({ "parameters": [] });
 }, "[]");
 
 test(() => {
-    assert_type({ "parameters": ["i32", "i64"] });
+  assert_type({ "parameters": ["i32", "i64"] });
 }, "[i32 i64]");
 
 test(() => {
-    assert_type({ "parameters": ["i32", "i64", "f32", "f64"] });
+  assert_type({ "parameters": ["i32", "i64", "f32", "f64"] });
 }, "[i32 i64 f32 f64]");
