1286f5f075b050750eaec1a65fc075397bcf2a79
Asumu Takikawa <asumu@igalia.com>
2021-07-06 10:03:13 -0700

    [interpreter] Add evaluation support for exceptions

--- interpreter/exec/eval.mli
+++ interpreter/exec/eval.mli
@@ -5,6 +5,7 @@ exception Link of Source.region * string
 exception Trap of Source.region * string
 exception Crash of Source.region * string
 exception Exhaustion of Source.region * string
+exception Uncaught of Source.region * string
 
 val init : Ast.module_ -> extern list -> module_inst (* raises Link, Trap *)
 val invoke : func_inst -> value list -> value list (* raises Trap *)
